166cd1fee31303fcdfe2d5a9650a9f64345fa0e6
taylor0417 <taylor57568@163.com>
2018-07-26 11:36:01 +0800

    服务端处理登录表单-表单校验

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,13 @@
+//mysql
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'maxin0608',
+  database : 'bbs'
+});
 
+connection.connect();
 
 //渲染用户登录的方法
 exports.showSignin = (req, res) => {
@@ -7,4 +16,38 @@ exports.showSignin = (req, res) => {
 //处理登录表单请求
 exports.handleSignin = (req, res) => {
   const body = req.body;
+  const sqlstr = 'SELECT * FROM `users` WHERE `email` =?';
+  connection.query(sqlstr,body.email,(err, results) => {
+    if(err) {
+      return res.send({
+        code:500,
+        message:err.message
+      });
+    }
+    if(!results[0]) {
+      return res.send({
+        code:1,
+        message:'邮箱不存在!'
+      });
+    }
+    const sqlstr = 'SELECT * FROM `users` WHERE `email` =? AND `password`=?';
+    connection.query(sqlstr,[body.email,body.password],(err, results) => {
+      if(err) {
+        return res.send({
+          code:500,
+          message:err.message
+        });
+      }
+      if(!results[0]) {
+        return res.send({
+          code:2,
+          message:'密码错误!'
+        });
+      }
+      res.send({
+        code:200,
+        message:'可以登录了'
+      });
+    });
+  });
 }
--- router.js
+++ router.js
@@ -4,8 +4,8 @@ const user = require('./controllers/user');
 const router = express.Router();
 
 //渲染登录页面的请求
-router.get('/signin', user.showSignin);
-      .post('/handleSignin', user.handleSignin);
+router.get('/signin', user.showSignin)
+      .post('/handleSignin', user.handleSignin)
 
 
 module.exports = router;
